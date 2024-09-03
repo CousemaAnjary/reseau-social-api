97036152b1eb7befbf94ed42777582551a96b482
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-03 19:41:15 +0300

    chore: Remove commented out code in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,8 +42,7 @@ export default function App() {
           <Route path="/friendzy" element={<DashboardClient />} />
           <Route path="/friendzy/profil" element={<Profil />} />
           <Route path="/friendzy/invitation" element={<InvitationClient />} />
-          {/* <Route path="/friendzy/invitation/:id" element={<InvitationClient />} /> */}
-          {/* fuck you ccccc */}
+         
         </Route>
 
 
